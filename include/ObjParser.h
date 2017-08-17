1e24f587435c6c05cb5c2685e9ccf206be759f3c
Unknown <heokyunam@naver.com>
2017-08-17 15:39:04 +0900

    텍스쳐링 완성
    결국 구조체를 사용해서 쉽게 처리를 하도록 만들었다
    각각 vertexIndex와 textureIndex, normalIndex대로
    쌓아놓은 결과 인덱싱을 잘못해서 생겼을 뿐
    그대로 하면 아무 문제도 없다는 얘기다

--- include/ObjParser.h
+++ include/ObjParser.h
@@ -1,8 +1,34 @@
 #pragma once
+#include "util.h"
 #include <vector>
 #include <GL/glut.h>
 
 using namespace std;
+
+struct Vector3;
+struct Material;
+struct Group;
+
+class ObjParser
+{
+private:
+	vector<Vector3> vertex, normal, tex;
+	vector<Group*> groups;
+	vector<Material*> materials;
+	Group* current;
+	Material* curMat;
+	
+	GLuint texture;
+public:
+	ObjParser();
+	~ObjParser();
+	void load(char* filename);
+	void input(char* buffer);
+	void draw();
+	bool loadTexture(char* filename);
+	void loadMaterial(char* filename);
+	void matInput(char* buffer);
+};
 
 struct Vector3 {
 	GLfloat x, y, z;
@@ -12,26 +38,23 @@ struct Vector3 {
 		this->z = z;
 	}
 };
-struct Vector2 {
-	GLfloat x, y, z;
-	Vector2(GLfloat x, GLfloat y) {
-		this->x = x;
-		this->y = y;
-	}
-};
-class ObjParser
-{
-private:
-	vector<Vector3> vertex, normal, tex;
-	vector<GLubyte> vIndices;
-	vector<Vector3> realVertex, realTex, realNormal;
-	GLuint texture;
-public:
-	ObjParser();
-	~ObjParser();
-	void load(char* filename);
-	void input(char* buffer);
-	void draw();
-	bool loadTexture(char* filename);
+
+struct Material {
+	char name[256];
+	int illum;
+	char map_Kd[256];
+	float Ns;
+	float Ni;
+	float d;
+	float Tr;
+	float Ka[3];
+	float Kd[3];
+	float Ks[3];
+	float Tf[3];
 };
 
+struct Group {
+	vector<Vector3> vertex, tex, normal;
+	Material* mat;
+	char name[20];
+};
--- src/ObjParser.cpp
+++ src/ObjParser.cpp
@@ -11,11 +11,20 @@
 
 ObjParser::ObjParser()
 {
+  current = 0;
 }
 
 
 ObjParser::~ObjParser()
 {
+  for(int i = 0; i < groups.size(); i++) {
+    Group* g = groups[i];
+    g->vertex.clear();
+    g->tex.clear();
+    g->normal.clear();
+    delete g;
+  }
+  groups.clear();
 }
 
 void ObjParser::load(char* filename)
@@ -32,7 +41,10 @@ void ObjParser::load(char* filename)
 	else {
 		cout << "load file fail" << endl;
 	}
-	
+	if(current) groups.push_back(current);
+	vertex.clear();
+	tex.clear();
+	normal.clear();
 }
 
 void ObjParser::input(char* buffer)
@@ -66,61 +78,57 @@ void ObjParser::input(char* buffer)
 			, &v[3], &uv[3], &nv[3]);
 		static int arr[] = { 1, 2, 3, 1, 3, 0 };
 		for (int i = 0; i < 6; i++) {
-			this->realVertex.push_back(vertex[v[arr[i]] - 1]);
-			this->realTex.push_back(tex[uv[arr[i]] - 1]);
-			this->realNormal.push_back(normal[nv[arr[i]] - 1]);
-			//for (int i = 0; i < 3; i++) {
-			//	this->realVertex.push_back(vertex[3 * (v[arr[i]] - 1) + i]);
-			//	this->realTex.push_back(tex[3 * (uv[arr[i]] - 1) + i]);
-			//}
-
-			this->vIndices.push_back(v[arr[i]] - 1);
+			current->vertex.push_back(vertex[v[arr[i]] - 1]);
+			current->tex.push_back(tex[uv[arr[i]] - 1]);
+			current->normal.push_back(normal[nv[arr[i]] - 1]);
 		}
-		/*
-		this->vIndices.push_back(v[1] - 1);
-		this->vIndices.push_back(v[2] - 1);
-		this->vIndices.push_back(v[3] - 1);
-
-		cout << v[1] - 1 << ", " << v[2] - 1 << ", " << v[3] - 1 << endl;
-
-		this->vIndices.push_back(v[1] - 1);
-		this->vIndices.push_back(v[3] - 1);
-		this->vIndices.push_back(v[0] - 1);
-
-		cout << v[1] - 1 << ", " << v[3] - 1 << ", " << v[0] - 1 << endl;
-		*/
 	}
 
 	else if (memcmp(buffer, "g ", 2) == 0) {
-
+		if(current) {
+		  groups.push_back(current);
+		}
+		current = new Group();
+		sscanf(buffer, "g %s", current->name);
 	}
 	else if (memcmp(buffer, "mtllib ", 7) == 0) {
-
+		char filename[20];
+		cout << "mtllib" << endl;
+		sscanf(buffer, "mtllib %s", filename);
+		sprintf(buffer, "box/%s", filename);
+		loadMaterial(buffer);
 	}
 	else if (memcmp(buffer, "usemtl ", 7) == 0) {
-
+		cout << buffer << endl;
+		char temp[20];
+		sscanf(buffer, "usemtl %s", temp);
+		for(int i = 0; i < materials.size(); i++) {
+		      if(strcmp(materials[i]->name, temp)) {
+			  current->mat = materials[i];
+			  break;
+		      }
+		}
 	}
 }
 
 
 void ObjParser::draw()
 {
-	//glBegin(GL_POLYGON);
-	GLubyte* indices = &vIndices[0];
-	float* v = (float*)&vertex[0];//&realVertex[0];//&vertex[0];
-	float* t = (float*)&realTex[0];
-	float* n = (float*)&realNormal[0];
-	glBindTexture(GL_TEXTURE_2D, texture);
+	for(int i = 0; i < groups.size(); i++) {
+		Group* g = groups[i];
+		float* v = (float*)&g->vertex[0];
+		float* t = (float*)&g->tex[0];
+		float* n = (float*)&g->normal[0];
+		
+		//glColor3f (g->mat->Kd[0], g->mat->Kd[1], g->mat->Kd[2]);
+		
+		glBindTexture(GL_TEXTURE_2D, texture);
+		glTexCoordPointer(3, GL_FLOAT, 0, t);
+		glNormalPointer(GL_FLOAT, 0, n);
 
-	glTexCoordPointer(3, GL_FLOAT, 0, t);
-	glNormalPointer(GL_FLOAT, 0, n);
-
-	//glVertexPointer(3, GL_FLOAT, 0, v);
-	//glDrawElements(GL_TRIANGLES, vIndices.size(), GL_UNSIGNED_BYTE, indices);
-
-	glVertexPointer(3, GL_FLOAT, 0, &realVertex[0]);
-	glDrawArrays(GL_TRIANGLES, 0, realVertex.size());
-	//glEnd();
+		glVertexPointer(3, GL_FLOAT, 0, v);
+		glDrawArrays(GL_TRIANGLES, 0, g->vertex.size());
+	}
 }
 
 bool ObjParser::loadTexture(char * filename)
@@ -139,4 +147,55 @@ bool ObjParser::loadTexture(char * filename)
 	cvReleaseImage (&img);
 	return true;
 
+}
+
+void ObjParser::loadMaterial(char* filename) {
+	ifstream openFile(filename);
+	char buffer[100];
+	memset(buffer, 0, 100);
+	if (openFile.is_open()) {
+		while (!openFile.eof()) {
+			openFile.getline(buffer, 100);
+			matInput(buffer);
+		}
+	}
+	else {
+		cout << "load material file fail" << endl;
+	}
+}
+
+void ObjParser::matInput(char* buffer) {
+	if(memcmp(buffer, "newmtl ", 7) == 0) {
+		if(curMat) {
+		    materials.push_back(curMat);
+		}
+		curMat = new Material();
+		
+		sscanf(buffer, "newmtl %s", curMat->name);
+	}
+	else if(memcmp(buffer, "Ns ", 3) == 0) {
+		sscanf(buffer, "Ns %f", &curMat->Ns);
+	}
+	else if(memcmp(buffer, "d ", 2) == 0) {
+		sscanf(buffer, "d %f", &curMat->d);
+	}
+	else if(memcmp(buffer, "Tr ", 3) == 0) {
+		sscanf(buffer, "Tr %f", &curMat->Tr);
+	}
+	else if(memcmp(buffer, "Tf ", 3) == 0) {
+		sscanf(buffer, "Tf %f %f %f", &curMat->Tf[0], &curMat->Tf[1], &curMat->Tf[2]);
+	}
+	else if(memcmp(buffer, "illum ", 6) == 0) {
+		sscanf(buffer, "illum %d", &curMat->illum);
+	}
+	else if(memcmp(buffer, "Ka ", 3) == 0) {
+		sscanf(buffer, "Ka %f %f %f", &curMat->Ka[0], &curMat->Ka[1], &curMat->Ka[2]);
+	}
+	else if(memcmp(buffer, "Kd ", 3) == 0) {
+		sscanf(buffer, "Kd %f %f %f", &curMat->Kd[0], &curMat->Kd[1], &curMat->Kd[2]);
+	}
+	else if(memcmp(buffer, "Ks ", 3) == 0) {
+		sscanf(buffer, "Ks %f %f %f", &curMat->Ks[0], &curMat->Ks[1], &curMat->Ks[2]);
+	}
+	
 }
